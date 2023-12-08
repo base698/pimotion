2fe4231783858ed91eb0b95ee52bcdc87125d33e
Justin Thomas <justin.thomas1@gmail.com>
2023-12-08 16:41:53 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -2,6 +2,7 @@ import time
 from gpiozero import MotionSensor
 from signal import pause
 from datetime import datetime
+import RPi.GPIO as GPIO
 
 pir = MotionSensor(4)
 
@@ -10,13 +11,26 @@ import queue
 
 q = queue.Queue()
 
+PIN_GPIO = 17
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PIN_GPIO, GPIO.OUT)
+
+def turn_off():
+    GPIO.output(PIN_GPIO, True)
+
+def turn_on():
+    GPIO.output(PIN_GPIO, True)
+
 def worker():
     while True:
         item = q.get()
         # TODO: actually turn on/off light
         if (datetime.now() - item).seconds < 30:
             print(f'turning off light {item}')
+            #turn_off()
             time.sleep(20)
+            #turn_on()
             print(f'turning on, Finished {item}')
         q.task_done()
 
@@ -29,3 +43,5 @@ pir.when_no_motion = lambda x: print(x,'no motion')
 threading.Thread(target=worker, daemon=True).start()
 
 pause()
+
+
