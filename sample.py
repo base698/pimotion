4d806aed1dfc29f9843324832b20f14353e3b0b1
Justin Thomas <justin.thomas1@gmail.com>
2023-12-11 16:37:50 -0500

    Update sample.py

--- sample.py
+++ sample.py
@@ -15,23 +15,28 @@ PIN_GPIO = 17
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIN_GPIO, GPIO.OUT)
+DELAY = 30
 
-def turn_off():
+# TODO: actually turn on/off light
+def turn_off(item):
+    print(f'turning off light {item}')
     GPIO.output(PIN_GPIO, True)
 
-def turn_on():
-    GPIO.output(PIN_GPIO, True)
+def turn_on(item):
+    print(f'turning on, Finished {item}')
+    GPIO.output(PIN_GPIO, False)
 
 def worker():
+    last_on = datetime.now()
     while True:
         item = q.get()
-        # TODO: actually turn on/off light
-        if (datetime.now() - item).seconds < 30:
-            print(f'turning off light {item}')
-            #turn_off()
-            time.sleep(20)
-            #turn_on()
-            print(f'turning on, Finished {item}')
+        if (item - last_on).seconds > DELAY:
+            turn_off(item)
+            time.sleep(DELAY)
+            turn_on(item)
+            last_on = item
+        else:
+            print('skipping')
         q.task_done()
 
 def on_motion(x):
@@ -40,8 +45,10 @@ def on_motion(x):
 pir.when_motion = on_motion
 pir.when_no_motion = lambda x: print(x,'no motion')
 
+print('starting...')
 threading.Thread(target=worker, daemon=True).start()
 
 pause()
+
 
 
